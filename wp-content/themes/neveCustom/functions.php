bdcdd5df8520d2a5648dc59014b2dbf2cdc9debb
al-burger <alex.burger08@gmail.com>
2023-05-05 08:36:07 +0200

    Init project

---
+++ wp-content/themes/neveCustom/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+if ( ! function_exists( 'neve_child_load_css' ) ):
+	/**
+	 * Load CSS file.
+	 */
+	function neve_child_load_css() {
+		wp_enqueue_style( 'neve-child-style', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'neve-style' ), NEVE_VERSION );
+	}
+endif;
+add_action( 'wp_enqueue_scripts', 'neve_child_load_css', 20 );
+
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), wp_get_theme()->get('Version'));
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
